5bae9a288cf753ebed0a28806a3bf470dfae22d1
pardo2410 <pardo2410@gmail.com>
2020-04-27 22:14:36 +0200

    modulos de baraja

---
+++ cartas.py
@@ -0,0 +1,38 @@
+palos= ['o','c','e','b']
+cartas=['A','2','3','4','5','6','7','S','C','R']
+
+
+def crea_baraja():
+
+    baraja=[]
+    for palo in palos:
+        for carta in cartas:
+            naipe=carta+palo
+            baraja.append(naipe)
+
+    return baraja
+
+
+
+'''
+#Ejercicio base
+p= ['o','c','e','b']
+c=['A','2','3','4','5','6','7','S','C','R']
+
+pf=['D','C','P','T']
+cf=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+
+def crea_baraja(palos,cartas):
+
+    baraja=[]
+    for palo in palos:
+        for carta in cartas:
+            naipe=carta+palo
+            baraja.append(naipe)
+
+    return baraja
+baraja1 = (crea_baraja(p,c))
+baraja2 = (crea_baraja(pf,cf))    
+print(baraja1)
+print(baraja2)
+'''
--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-p= ['o','c','e','b']
-c=['A','2','3','4','5','6','7','S','C','R']
-
-pf=['D','C','P','T']
-cf=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-
-def crea_baraja(palos,cartas):
-
-    baraja=[]
-    for palo in palos:
-        for carta in cartas:
-            naipe=carta+palo
-            baraja.append(naipe)
-
-    return baraja
-baraja1 = (crea_baraja(p,c))
-baraja2 = (crea_baraja(pf,cf))    
-print(baraja1)
-print(baraja2)
-
+import cartas
+print(cartas.crea_baraja())
