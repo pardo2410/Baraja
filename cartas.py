90e34eb2a5838e4d888af9ee6f5668bedf0f25ac
pardo2410 <pardo2410@gmail.com>
2020-04-28 20:39:12 +0200

    Segunda cata

--- cartas.py
+++ cartas.py
@@ -1,3 +1,4 @@
+import random
 palos= ['o','c','e','b']
 cartas=['A','2','3','4','5','6','7','S','C','R']
 
@@ -12,27 +13,35 @@ def crea_baraja():
 
     return baraja
 
+def mezclar(b):
+    i=0
+    baraja_retorno=[]
+    while i < 40:
+        n = random.randint(0,39)
+        while b[n] in baraja_retorno:
+            n = random.randint(0,39)
+        baraja_retorno.append(b[n])
+        i += 1
+    b = baraja_retorno
+    return b
 
+def reparte(b, players, cards,metodo=False):
+    res = []
+    
+    if metodo == False:
+        for p in range(players):
+            res.append([])
+        for ic in range(cards):
+            for ij in range(players):
+                carta = b.pop(0)
+                res[ij].append(carta)
+        return res
+    else:
+        for p in range(players):
+            res.append([])
+            for ij in range(players):
+                for ic in range(cards):
+                    carta = b.pop(0)
+                    res[ic].append(carta)
+        return res
 
-'''
-#Ejercicio base
-p= ['o','c','e','b']
-c=['A','2','3','4','5','6','7','S','C','R']
-
-pf=['D','C','P','T']
-cf=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-
-def crea_baraja(palos,cartas):
-
-    baraja=[]
-    for palo in palos:
-        for carta in cartas:
-            naipe=carta+palo
-            baraja.append(naipe)
-
-    return baraja
-baraja1 = (crea_baraja(p,c))
-baraja2 = (crea_baraja(pf,cf))    
-print(baraja1)
-print(baraja2)
-'''
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import cartas
-print(cartas.crea_baraja())
+b1 = cartas.crea_baraja()
+bm = cartas.mezclar(b1)
+print(bm)
+Jugadores = int(input('Numero de jugadores: '))
+cartas_j = int(input('Numero de cartas: '))
+metodo_r = input('Desea cambiar el metodo de repartir S/N: ')
+if metodo_r =='N':
+    metodo_r = False
+else:
+    metodo_r = True
+print(cartas.reparte(bm,Jugadores,cartas_j,metodo_r))
